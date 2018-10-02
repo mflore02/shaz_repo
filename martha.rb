9ad0cbf9efbfd3da97be576dd050b74bfc11ee96
“Martha” <martha.dfwrealtor@gmail.com>
2018-10-02 09:47:57 -0500

    yo what up doc

---
+++ martha.rb
@@ -0,0 +1,79 @@
+# num = 1
+# while num < 11
+# 	puts num
+# 	num += 1
+	
+# end
+
+# num = 1
+# while num < 11
+# 	puts num
+# 	num += 1
+# end
+
+# num = 1
+# while num < 25
+	# puts num
+	# num += 2
+# end
+
+# num = 25
+# while num > 1
+# 	puts num
+# 	num -= 3
+# end
+# my_array = ["Maria", "Judy", "Brad", "Samia","Jaleel","Ches","One","Two","Jacob"]
+
+# my_array.each  do |name|
+# 	puts name
+	
+# end
+
+# peeps = ["stephanie","Judy","Samia","Jacob"]
+# peeps.each_with_index do [index,1]
+# puts "#{stephanie} is number #{Samia+1}!"
+# end
+
+# def wumbo
+# 	puts "here's my little game"
+# 	puts x = 1 + 3 
+# end
+
+# wumbo()
+
+# put "give me a number to my pounds to kilograms"
+# 2.2 pound = gets.chomp.to_i
+# puts "give me one kilograms
+
+# y = [[1,2],[3,4]].to_h
+
+# puts y
+
+
+
+
+
+
+# y = [[2.0,"hi"],[5.0,"hi"]].to_h
+
+# puts y
+
+# phrase = "Giraffe Academy"
+# puts phrase.include? "Academy"
+puts "Enter a color"
+color = gets.chomp()
+puts "Enter plural_noun"
+plural_noun = gets.chomp()
+puts "Enter a name"
+name = gets.chomp()
+
+puts ("Roses are"+color)
+puts (plural_noun+"are blue")
+puts ("I love"+name)
+
+
+
+
+
+
+
